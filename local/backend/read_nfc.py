9b78178062cf1ac1eddea47cde8ab0c9a5e0424a
Jan Sellerbeck <henry4711lp@gmail.com>
2025-11-01 22:08:59 +0100

    Refactor NFC module import and update requirements.txt for RPi.GPIO

--- local/backend/read_nfc.py
+++ local/backend/read_nfc.py
@@ -1,7 +1,7 @@
 import board
 import busio
 from digitalio import DigitalInOut
-import adafruit_pn532
+from adafruit_pn532.spi import PN532_SPI
 
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 cs_pin = DigitalInOut(board.D5)  # anpassen, je nach Verdrahtung
