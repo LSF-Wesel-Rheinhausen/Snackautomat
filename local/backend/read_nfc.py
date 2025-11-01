015cccd6df522698bb2ae4303d337f4ea854cc34
Jan Sellerbeck <henry4711lp@gmail.com>
2025-11-01 22:39:18 +0100

    Refactor NFC module import and update requirements.txt for RPi.GPIO

--- local/backend/read_nfc.py
+++ local/backend/read_nfc.py
@@ -1,13 +1,11 @@
 import subprocess
 
 def read_uid():
-    result = subprocess.run(
-        ["nfc-poll"], capture_output=True, text=True
-    )
-    for line in result.stdout.splitlines():
-        if "UID" in line:
-            uid = line.split(":")[1].strip().replace(" ", "").upper()
-            return uid
-    return None
+    p1 = subprocess.Popen(["nfc-poll"], stdout=subprocess.PIPE, text=True)
+    p2 = subprocess.Popen(["awk", "/UID/ {print $3$4$5$6$7$8$9; exit}"], stdin=p1.stdout, stdout=subprocess.PIPE,
+                          text=True)
+    p1.stdout.close()
+    out = p2.communicate()[0].strip()
+    return out or None
 
 print(read_uid())
--- local/backend/read_uid.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nfc-poll | awk '/UID/ {print $3$4$5$6$7$8$9; exit}'
